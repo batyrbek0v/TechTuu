958a06a428f231a8a4a367ae56d56fad4e3792df
Эрлан Камбаров <erlankambarov94@gmail.com>
2022-12-22 18:41:50 +0600

    added projects section and slider for mobile version

--- JS/script.js
+++ JS/script.js
@@ -3,8 +3,58 @@
 const menuBurger = document.querySelector('.burger')
 const mobileWrapper = document.querySelector('.mobile-wrapper')
 const closeIcon = document.querySelector('.close-img')
+const slider = document.querySelector('.slider')
 
 // DOM-ELEMENTS-END
+
+const sliderBase = [
+  {
+    img: './assets/img/project-img2.png',
+    route: '/'
+  },
+  {
+    img: './assets/img/project-img3.png',
+    route: '/'
+  },
+  {
+    img: './assets/img/project-img4.png',
+    route: '/'
+  },
+  {
+    img: './assets/img/project-img5.png',
+    route: '/'
+  },
+  {
+    img: './assets/img/project-img6.png',
+    route: '/'
+  },
+  {
+    img: './assets/img/project-img1.png',
+    route: '/'
+  },
+  {
+    img: './assets/img/project-img7.png',
+    route: '/'
+  },
+]
+
+window.addEventListener('load', () => {
+  sliderCardsTemplate(sliderBase)
+})
+
+const sliderCardsTemplate = sliderBase => {
+  const template = sliderBase.map(item => {
+    return `
+        <a href="${item.route}">
+          <div class="project-block">
+            <img src="${item.img}" alt="">
+          </div>
+        </a>
+    `
+  }).join('')
+
+  slider.innerHTML = template
+}
 
 menuBurger.addEventListener('click', e => {
   e.preventDefault()
@@ -17,3 +67,19 @@ closeIcon.addEventListener('click', e => {
   e.preventDefault()
   mobileWrapper.classList.remove('active')
 })
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: "3",
+  spaceBetween: 20,
+  breakpoints: {
+    '320': {
+      slidesPerView: 1,
+    },
+    '450': {
+      slidesPerView: 2,
+    },
+    '768': {
+      slidesPerView: 3,
+    },
+  },
+});
